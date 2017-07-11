88b9e7d2681e87d84fad3e64d65fc28897961b03
Unknown <jtheberge@outlook.com>
2017-07-11 14:43:06 -0700

    Changed server root path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, 'build/index.html'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build/index.html'))
