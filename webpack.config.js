1dbdef3852872fdd99a4e380b89979c668869df3
Unknown <jtheberge@outlook.com>
2017-07-18 00:06:54 -0700

    Enable async of transformed.js created by webpack
    Install package script-ext-htmp-webpack-plugin to add async option to transformed js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var path = require('path');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
 var HTMLWebpackPLuginConfig = new HTMLWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
+});
+
+var ScriptExtHtmlWebpackPluginConfig = new ScriptExtHtmlWebpackPlugin({
+  defaultAttribute: 'async'
 });
 
 module.exports = {
@@ -18,14 +24,14 @@ module.exports = {
       }
     ]
   },
+  
   output: {
     filename: 'scripts/transformed.js',
     path: __dirname + '/build',
     publicPath: '/'
   },
 
-
-  plugins: [HTMLWebpackPLuginConfig],
+  plugins: [HTMLWebpackPLuginConfig, ScriptExtHtmlWebpackPluginConfig],
 
   devServer: {
     contentBase: path.join(__dirname, 'public'),
