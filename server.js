dfbdae31237bc017ac0d051921c54da72c668474
Unknown <jtheberge@outlook.com>
2017-07-17 22:57:26 -0700

    Fix compression

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(compression());
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/public'));
-app.use(compression());
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build/index.html'))
