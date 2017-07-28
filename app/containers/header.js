c0b9ffd9f542e85ead6093289777db6198dce663
Unknown <jtheberge@outlook.com>
2017-07-28 14:06:01 -0700

    Add sign out (delUserInfo action). Move sign in button for cleaner code.
    Reducer_userinfo handles deleting store data provided from Oauth.

--- app/containers/header.js
+++ app/containers/header.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import { fetchSearch, setUserInfo } from '../actions';
+import { fetchSearch, setUserInfo, delUserInfo } from '../actions';
 import GoogleLogin from 'react-google-login';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
-  checkImg(url){
-    if (url) { return (
-    <img className="oauth-img" src={url} alt="Profile photo" />
+  displaySignedIn(userInfo, delUserInfo){
+    if (userInfo) { return (
+      <span>
+        <img className="oauth-img" src={userInfo.imageUrl} alt="Profile photo" />
+        <button type="button" onClick={delUserInfo} className="signOutBtn">Sign Out</button>
+      </span>
   )}
     else return;
   }
+
   renderField(field) {
     const { meta: { touched, error } } = field;
-    const searchBarClass = `input-group ${touched && error ? 'has-danger' : ''}`;
-    const oAuthClass = `oauth-btn ${field.userInfo.imageUrl ? 'withImg' : ''}`;
-    const textHelpClass = `${touched && error ? 'text-help' : ''}`;
+    const searchBarClass = `col-md-8 mx-auto input-group ${touched && error ? 'has-danger' : ''}`;
+    const textHelpClass = `${touched && error ? 'text-help offset-md-2' : ''}`;
     return (
       <div>
         <label className="sr-only">{field.label}</label>
         <div className={searchBarClass}>
           <input
-            className="col-md-8 offset-md-2 form-control"
+            className="form-control"
             type="text"
             placeholder='Search for lecturer or class'
             {...field.input}
@@ -34,21 +37,6 @@ class Header extends Component {
           <div className={textHelpClass}>
             {touched ? error : ''}
           </div>
-        </div>
-        <div style={{marginTop: "7px"}}>
-          <Link to={'/'}>
-            <i className="fa fa-home homeBtn"></i>
-          </Link>
-          <GoogleLogin
-            hostedDomain="scu.edu"
-            clientId="471296732031-0hqhs9au11ro6mt87cpv1gog7kbdruer.apps.googleusercontent.com"
-            buttonText={field.userInfo.givenName}
-            onSuccess={field.setInfo}
-            onFailure={field.setInfo}
-            style={{}}
-            className={oAuthClass}
-          />
-          {field.checkImg(field.userInfo.imageUrl)}
         </div>
       </div>
     );
@@ -64,19 +52,33 @@ class Header extends Component {
 
   render() {
     const { handleSubmit, setUserInfo, userInfo } = this.props;
+    const oAuthClass = `oauth-btn ${userInfo != null ? 'withImg' : ''}`;
     return (
       <header>
+
       <form className="container" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Read &amp; Write SCU Evals"
           name="searchBar" //responsible for object's key name for values
           component={this.renderField}
-          setInfo={setUserInfo}
-          userInfo={userInfo}
-          checkImg={this.checkImg}
-
         />
       </form>
+
+      <div className="container" style={{marginTop: "7px"}}>
+        <Link to={'/'}>
+          <i className="fa fa-home homeBtn"></i>
+        </Link>
+        <GoogleLogin
+          hostedDomain="scu.edu"
+          clientId="471296732031-0hqhs9au11ro6mt87cpv1gog7kbdruer.apps.googleusercontent.com"
+          buttonText={userInfo ?  userInfo.givenName: 'Sign in with SCU Gmail'}
+          onSuccess={setUserInfo}
+          onFailure={setUserInfo}
+          style={{}}
+          className={oAuthClass}
+        />
+        {this.displaySignedIn(userInfo, this.props.delUserInfo)}
+      </div>
       </header>
     );
   }
@@ -100,5 +102,5 @@ export default reduxForm({
   validate,
   form: 'searchBar'
 })(
-  connect(mapStateToProps,{ setUserInfo, fetchSearch })(Header)
+  connect(mapStateToProps,{ setUserInfo, delUserInfo, fetchSearch })(Header)
 );
